e4efd30cead0fad5f7d85f695378399d03583765
alexgeleisha <121766149+alexgeleisha@users.noreply.github.com>
2026-04-01 12:34:20 +0300

    Add load function

--- finance/storage.py
+++ finance/storage.py
@@ -2,3 +2,9 @@ import json
 def save(data, filename="data.json"):
     with open(filename, "w") as f:
         json.dump(data, f)
+def load(filename="data.json"):
+    try:
+        with open(filename) as f:
+            return json.load(f)
+    except:
+        return []
