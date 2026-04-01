efa70e18c6dc8c1bc0c829abb74d7ae1e48243d5
alexgeleisha <121766149+alexgeleisha@users.noreply.github.com>
2026-04-01 12:37:14 +0300

    Add formatted output

--- finance/storage.py
+++ finance/storage.py
@@ -16,3 +16,5 @@ def get_expenses():
     return [t for t in transactions if t["amount"] < 0]
 def get_income():
     return [t for t in transactions if t["amount"] > 0]
+def format_transactions():
+    return [f"{t['name']} {t['amount']}" for t in transactions]
